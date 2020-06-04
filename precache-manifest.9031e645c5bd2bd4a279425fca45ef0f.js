a5589d5bc8d1f6faba93ed469289be05f65409ee
rende99 <rende99@gmail.com>
2020-06-04 13:52:52 -0400

    new project

---
+++ precache-manifest.9031e645c5bd2bd4a279425fca45ef0f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac64e3272c450fd08b2dfd3c0149240",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59cc7b21f6be5497f52c",
+    "url": "/static/css/2.5fbf1059.chunk.css"
+  },
+  {
+    "revision": "ff05139dd2a740923de0",
+    "url": "/static/css/main.c5e54ddd.chunk.css"
+  },
+  {
+    "revision": "59cc7b21f6be5497f52c",
+    "url": "/static/js/2.bc8c783b.chunk.js"
+  },
+  {
+    "revision": "b578178f33e8b4baed76c7b8f7ce2e07",
+    "url": "/static/js/2.bc8c783b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff05139dd2a740923de0",
+    "url": "/static/js/main.ae736c97.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/static/media/icons-20.cef8cdbb.woff"
+  },
+  {
+    "revision": "a3dc2a53e326b0cb662be9f09309d9be",
+    "url": "/static/media/sweet_purple.a3dc2a53.ttf"
+  }
+]);
